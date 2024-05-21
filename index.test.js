c9fad242ffc05218eba9f8392477251b59ee93b8
MrOxMasTer <59291123+MrOxMasTer@users.noreply.github.com>
2024-05-21 21:48:43 +0300

    first commit

---
+++ index.test.js
@@ -0,0 +1,28 @@
+const postcss = require("postcss");
+const { equal } = require("node:assert");
+const { test } = require("node:test");
+
+const plugin = require("./");
+
+async function run(input, output, opts = {}) {
+  let result = await postcss([plugin(opts)]).process(input, {
+    from: undefined,
+  });
+
+  equal(result.css, output);
+  equal(result.warnings().length, 0);
+}
+
+test("does something", async () => {
+  await run(
+    "@apply mm:(bg-redOrange,text-green)",
+    "@apply mm:bg-redOrange mm:text-green",
+    {}
+  );
+});
+
+/* Write tests here
+
+
+
+*/
